07a3292a61697e3581d6a05e2a6feb687ed1db3c
DanielAKidd <danfishermoon@yahoo.co.uk>
2016-09-02 16:33:40 +0100

    completed var solutions

---
+++ intro/var_solutions/name.rb
@@ -0,0 +1,12 @@
+puts "What is your name?"
+name = gets.chomp
+puts "Welcome #{name}"
+
+
+10.times {puts name}
+
+puts "What is your first name?"
+fname = gets.chomp
+puts "What is your surname?"
+lname = gets.chomp
+puts "Hello again #{fname} #{lname}"

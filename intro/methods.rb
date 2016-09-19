aa30d4b08c66aa49e6be43cc229178394fe4388d
DanielAKidd <danfishermoon@yahoo.co.uk>
2016-09-19 22:13:22 +0100

    added solutions to method chapter

---
+++ intro/methods.rb
@@ -0,0 +1,37 @@
+# exercise 1
+def greeting name
+  puts "Hello #{name}"
+end
+
+# puts greeting "Daniel"
+
+# exercise 2
+# 1. 2
+# 2. nil
+# 3. "Joe"
+# 4. "four"
+# 5. nil
+
+# exercise 3
+def multiply n1, n2
+  n1 * n2
+end
+
+# puts multiply 3, 4
+
+# exercise 4
+# Nothing is returned
+
+# exercise 5
+def scream words
+  words = words + "!!!!"
+  puts words
+end
+
+scream "Can you hear me"
+
+# method returns 'nil' as last line is return value - unless keyword 'return' is used -
+# and that is from the puts method.
+
+# exercise 6
+# method was expecting 2 arguments but got one

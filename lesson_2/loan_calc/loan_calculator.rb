93bab845e8e1041ae789045cbb3aa75ce7e90af0
DanielAKidd <danfishermoon@yahoo.co.uk>
2016-10-13 18:18:03 +0100

    add loan calc

--- lesson_2/loan_calc/loan_calculator.rb
+++ lesson_2/loan_calc/loan_calculator.rb
@@ -1,3 +1,6 @@
+require 'yaml'
+MESSAGES = YAML.load_file('msg_config.yml')
+
 def prompt(msg)
   puts("==> #{msg}")
 end
@@ -17,7 +20,7 @@ def fetch_loan_amount
     if float?(num)
       return num.to_f
     else
-      #prompt MESSAGES['num_fail']
+      prompt MESSAGES['num_fail']
     end
   end
 end
@@ -29,7 +32,7 @@ def fetch_apr
     if float?(num)
       return num.to_f
     else
-      # fail msg
+      prompt MESSAGES['apr_fail']
     end
   end
 end
@@ -41,54 +44,59 @@ def fetch_duration
     if integer?(num)
       return num.to_i
     else
-      # fail msg
+      prompt MESSAGES['duration_fail']
     end
   end
 end
 
 def calc_monthly_interest_rate(apr)
-  apr/12.0
+  ((apr / 12) / 100)
 end
 
 def calc_duration_in_months(duration)
-  duration*12
+  duration * 12
 end
 
 def calc_monthly_payments(p, j, n)
-  p * (j / (1 - (1 + j)**-n))
+  r = p * (j / (1 - (1 + j)**-n))
+  r.round(2)
 end
 
 def display
 end
 
 # MSG: welcome
+prompt MESSAGES['welcome']
 
 loop do
   # MSG: ask for loan amount
+  prompt MESSAGES['loan_amount_prompt']
   loan_amount = fetch_loan_amount
 
   # MSG: ask for APR (annual percentage rate) denominated in 10s 10 = > 10%
+  prompt MESSAGES['apr_prompt']
   apr = fetch_apr
 
   # MSG: ask loan duration
+  prompt MESSAGES['loan_duration_prompt']
   loan_duration = fetch_duration
 
   # calc monthly interest rate
-  monthly_interest_rate = calc_monthly_interest_rate
+  monthly_interest_rate = calc_monthly_interest_rate(apr)
   # calc loan duration in months
-  duration_in_months = calc_duration_in_months
+  duration_in_months = calc_duration_in_months(loan_duration)
 
   # calc monthly payments
-  monthly_payments = calc_monthly_payments(loan_amount, 
-                                           monthly_interest,
+  monthly_payments = calc_monthly_payments(loan_amount,
+                                           monthly_interest_rate,
                                            duration_in_months)
-
   # display data to user
+  prompt(monthly_payments)
 
   # check if user wants another calculation
-  #prompt MESSAGES['replay']
+  prompt MESSAGES['replay']
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-# MSG: farewell
+prompt MESSAGES['goodbye']
